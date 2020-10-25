ae73b6510fd6d15ff5da95170a9764dda5ed20ab
lisong <55542271+glassy-sky-lisong@users.noreply.github.com>
2020-10-25 22:16:54 +0800

    1

--- StudentMgr/studentSystem.go
+++ StudentMgr/studentSystem.go
@@ -1,11 +1,10 @@
 package StudentMgr
 
 type student struct {
-  name string
-  age int64
+	name string
+	age  int64
 }
 
-func NewStudent(name string, age int64) stu *student {
-  return &student{name, age}
+func NewStudent(name string, age int64) (stu *student) {
+	return &student{name, age}
 }
-
